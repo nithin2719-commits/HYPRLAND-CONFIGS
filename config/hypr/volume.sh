ef8a45b54c4324edb4405231bd2f0d9b3370be12
nithin2719-commits <thangamnithin1980@gmail.com>
2026-05-15 00:56:07 +0530

    hypr: set exec ~/.local/share/bin/vol-osd.sh "$@"
    config/hypr/volume.sh (1/1)

--- config/hypr/volume.sh
+++ config/hypr/volume.sh
@@ -1,15 +1,2 @@
 #!/usr/bin/env bash
-
-case $1 in
-  up) pamixer -i 5 ;;
-  down) pamixer -d 5 ;;
-  mute) pamixer -t ;;
-esac
-
-VOL=$(pamixer --get-volume)
-
-eww update volume=$VOL
-eww open volume
-
-sleep 1
-eww close volume
+exec ~/.local/share/bin/vol-osd.sh "$@"
